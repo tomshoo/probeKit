661d0507b02232143a3154fb27996c7eb5196adb
theEndurance-del <tomarshubhanshu7@gmail.com>
2021-05-18 12:04:14 +0530

    using nmap library to perform active OS fingerprinting on the target

--- modules/probe/osprobe.py
+++ modules/probe/osprobe.py
@@ -1,35 +1,71 @@
 from scapy.all import *
+from colorama import Fore, init, Back
+import nmap
+import json
+import socket
 
-def __getTTL(target, iterator):
+FRED = Fore.RED
+FWHITE = Fore.WHITE
+FGREEN = Fore.GREEN
+FYELLOW = Fore.YELLOW
+
+def checkTTL(target, iterator):
 	pkt = IP(dst=target)/ICMP(seq=9999)
-	pktlist = []
+	ttllist = []
 
-	for _ in range(0, iterator):
+	if iterator == 0:
+		raise Exception(f'{FRED}Error: invalid try count specified: {iterator}{FWHITE}')
+	
+	for _ in range(iterator):
 		rpkt = sr1(pkt)
-		if rpkt:
-			pktlist.append(rpkt.ttl)
+		rpttl = rpkt[IP].ttl
+		ttllist.append(rpttl)
 
-	return pktlist
+	return ttllist
 
-def checkOS(target, iterator):
-	ttllist = __getTTL(target, iterator)
-	def checkTTL(ttllist):
-		for x in ttllist:
-			print(f'observed TTL: {x}')
-			if x < 64:
-				return 'Linux'
+class checkOS():
+	nmapscanner = nmap.PortScanner()
+	def __init__(self, target, iterator, ifnmap):
+		self.target = target
+		self.iterator = iterator
+		self.ifnmap = ifnmap
 
-			elif x < 128:
-				return 'DOS'
+	def OSbyTTL(self):
+		ttllist = checkTTL(self.target, self.iterator)
+		for i in ttllist:
+			if i <= 64:
+				return "Linux"
 
-			elif x < 256:
-				return 'Solaris'
+			elif i <= 128:
+				return "DOS"
 
-	if checkTTL(ttllist) == 'Linux':
-		print(f'Host possibly running Linux based OS')
+			elif i <= 256:
+				return "Solaris"
 
-	elif checkTTL(ttllist) == 'DOS':
-		print(f'Host possibly running Windows or other DOS based OS')
+	def nmapScan(self):
+		scanner = self.nmapscanner
+		target = self.target
+		targetip = socket.gethostbyname(target)
 
-	elif checkTTL(ttllist) == 'Solaris':
-		print(f'Host possibly running Solaris based OS')
+		OSresult = scanner.scan(hosts=target, arguments="-O")['scan'][targetip]['osmatch']
+		if not OSresult:
+			print(FRED+"Error: Unable to identify OS via Nmap")
+		else:
+			print(OSresult)
+
+	def scanner(self):
+		print("Running TTL check on host")
+		print("Please wait...")
+
+		HostOSbyTTL = self.OSbyTTL()
+		print(f"{FGREEN}[*] Host appears to be running {HostOSbyTTL} based OS{FWHITE}")
+
+		if self.ifnmap == 1:
+			print("Please wait running Nmap scan...")
+			self.nmapScan()
+
+		elif self.ifnmap == 0:
+			print(f'{FYELLOW}Alert: Skipping nmap scan')
+
+		else:
+			raise Exception(f'{FRED}Error: invalid condition for nmap: {self.ifnmap}{FWHITE}')
