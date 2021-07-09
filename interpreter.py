a35853060aaad0f38e5a762c5b1d45c9811e3595
theEndurance-del <tomarshubhanshu7@gmail.com>
2021-07-09 11:44:58 +0530

    Implementing aliases, up arrow history navigation

--- interpreter.py
+++ interpreter.py
@@ -6,7 +6,7 @@ import sys
 import mod_interpreter as modinterpreter
 import modules.data.OptInfHelp as data
 import modules.data.AboutList as Module
-from data import colors
+from data import colors, aliases
 
 FSUCCESS = colors.FSUCCESS
 FALERT = colors.FALERT
@@ -45,6 +45,9 @@ exitStatus = FSUCCESS+'0'
 try:
     while True:
         value = input(FNORMAL+'[probkit]:'+f' {exitStatus}'+FNORMAL+'$> ')
+        
+        if value in aliases['command_aliases']:
+            value = aliases['command_aliases'][value]
 
         commandSplit = value.split()
 
@@ -54,22 +57,22 @@ try:
         elif value[0] == '#':
             exitStatus = FSUCCESS+'0'
 
-        elif value in ['exit', 'terminate']:
+        elif value == 'exit' or value in aliases['exit']:
             sys.exit()
 
-        elif value == "help":
+        elif value == "help" or value in aliases['help']:
             exitStatus = FSUCCESS+'0'
             Data = data.Help('')
             Data.showHelp()
 
-        elif value == "list":
+        elif value == "list" or value in aliases['list']:
             exitStatus = FSUCCESS+'0'
             Mod.listmodules()
 
-        elif value == "banner":
+        elif value == "banner" or value in aliases['banner']:
             banner()
 
-        elif __returnval(commandSplit, 0) == 'clear':
+        elif __returnval(commandSplit, 0) == 'clear' or __returnval(commandSplit, 0) in aliases['clear']:
             print(chr(27)+'2[j')
             print('\x33c')
             print('\x1bc')
@@ -77,7 +80,7 @@ try:
             if __returnval(commandSplit, 1) in ['exit', 'terminate']:
                 sys.exit()
 
-        elif __returnval(commandSplit, 0) == 'use':
+        elif __returnval(commandSplit, 0) == 'use' or __returnval(commandSplit, 0) in aliases['use']:
             if __returnval(commandSplit, 1) in Module.moduleHelp.modules:
                 exitStatus = FSUCCESS+'0'
                 modinterpreter.interpreter(__returnval(commandSplit, 1))
@@ -88,7 +91,7 @@ try:
                 print(FALERT+'Error: Invalid module specified')
                 exitStatus = FALERT+'1'
 
-        elif __returnval(commandSplit, 0) == 'about':
+        elif __returnval(commandSplit, 0) == 'about' or __returnval(commandSplit, 0) in aliases['about']:
             if __returnval(commandSplit, 1):
                 mod = __returnval(commandSplit, 1)
                 Module.moduleHelp(mod).aboutModule(mod)
