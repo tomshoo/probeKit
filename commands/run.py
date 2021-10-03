aedd06fe6e906afbca580fcf2af5f79e4bf5d4a5
theEndurance-del <tomarshubhanshu@protonmail.ch>
2021-10-03 16:15:17 +0530

    Optimized `run.py`

--- commands/run.py
+++ commands/run.py
@@ -1,17 +1,16 @@
-from modules.data.AboutList import moduleHelp
-from config import colors, variables
-from modules.util.utils import isFloat
+from modules.data.AboutList import moduleHelp as _modulehelp
+from config import colors as _colors
+import modules.probe.ports as _ports
+import modules.probe.osprobe as _osprobe
 
-FALERT = colors.FALERT
-BALERT = colors.BALERT
-BNORMAL = colors.BNORMAL
+_FALERT = _colors.FALERT
+_BALERT = _colors.BALERT
+_BNORMAL = _colors.BNORMAL
 
 
 def run(module, options) -> int:
     """Function to run the assigned module"""
-    import modules.probe.ports as ports
-    import modules.probe.osprobe as osprobe
-    if module in moduleHelp(module).modules:
+    if module in _modulehelp(module).modules:
         thost    = options[0]
         tport    = options[1]
         protocol = options[2]
@@ -21,19 +20,19 @@ def run(module, options) -> int:
         verbose  = options[6]
         threading= options[7]
         if thost == '':
-            print(FALERT+'Error: Invalid value for THOST')
+            print(_FALERT+'Error: Invalid value for THOST')
         else:
             if module == 'probe':
                 if tport == '':
-                    print(FALERT+'Error: Invalid value for TPORT')
+                    print(_FALERT+'Error: Invalid value for TPORT')
 
-                ports.display(thost, tport, timeout, protocol, tryct, verbose, threading)
+                _ports.display(thost, tport, timeout, protocol, tryct, verbose, threading)
                 return 0
 
             elif module == 'osprobe':
-                osprobe.checkOS(thost, tryct, nmap).scanner()
+                _osprobe.checkOS(thost, tryct, nmap).scanner()
                 return 0
 
     else:
-        print(f'{BALERT}[-] Error: Invalid module \'{module}\'{BNORMAL}')
+        print(f'{_BALERT}[-] Error: Invalid module \'{module}\'{_BNORMAL}')
         return 1
