eaaf3d41ff4637491a7e7b6dc9b27695ce5db669
theEndurance-del <tomarshubhanshu@protonmail.ch>
2022-04-10 23:10:45 +0530

    New `dir` command for Windows

---
+++ commands/directory.py
@@ -0,0 +1,6 @@
+import subprocess
+from modules.util.CommandUtils.ReturnStructure import RetObject
+
+def run(arguments: list[str], ReturnObject: RetObject) -> RetObject:
+    ReturnObject.exit_code = subprocess.run(['dir']+arguments, shell=True).returncode
+    return ReturnObject
