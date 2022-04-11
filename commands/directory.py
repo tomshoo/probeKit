a3efeaeb26755b227aa835c51117732a1db3c8c2
theEndurance-del <tomarshubhanshu@protonmail.ch>
2022-04-11 16:44:28 +0530

    Clean stuff a little

--- commands/directory.py
+++ commands/directory.py
@@ -2,5 +2,8 @@ import subprocess
 from modules.util.CommandUtils.ReturnStructure import RetObject
 
 def run(arguments: list[str], ReturnObject: RetObject) -> RetObject:
-    ReturnObject.exit_code = subprocess.run(['dir']+arguments, shell=True).returncode
+    ReturnObject.exit_code = subprocess.run(
+        ['dir']+arguments,
+        shell=True
+    ).returncode
     return ReturnObject
